a00e6d0239c4fd53c56e6ca28b1bdb8fc4eef087
jpVinnie <janpaul.ramos@protonmail.com>
2023-04-22 16:15:36 -0400

    limit to the 30 most recent commits

--- scripts/sha.py
+++ scripts/sha.py
@@ -12,7 +12,7 @@ log = subprocess.run(
 shas = list(reduce(lambda a, s: a + s.split('\t'), log.stdout.split('\n')[:-1], []))[::2]
 
 sha_file = open(f"{base_name}-sha.txt", "w+")
-for i in range(len(shas)):
+for i in range(31):
     sha_file.write(f"{git_url} {shas[i]} {base_name + str(i)}\n")
 
 sha_file.close()
