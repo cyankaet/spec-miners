dd6601502e5c546a4fcbbdc69a6b24e9b935c3db
jpVinnie <janpaul.ramos@protonmail.com>
2023-04-29 16:59:40 -0400

    Javert finished on jtar, tables for bdd

--- scripts/classification.py
+++ scripts/classification.py
@@ -1,25 +1,44 @@
-# This classification script separates mined specifications into either:
-# Persistent, the specification stays during the entirety of the repos lifespan
-# Intermittent, the specification appears and disappears at different stages
-# Disappearing, the specification appears at a single stage and disappears for the rest
+'''
+This classification script separates mined specifications into either:
+Persistent, the specification stays during the entirety of the repos lifespan
+Intermittent, the specification appears and disappears at different stages
+Disappearing, the specification appears at a single stage and disappears for the rest
+'''
 
 import os
+import re
+import random
 
-miner_dir = input("Please specify the miner specs you wish to classify (b/j/t/d): ")
+'''Lookup table for each spec and its respective miner'''
+b_table = {'BDD': None}
+j_table = {'Javert': None}
+t_table = {'Texada': None}
+d_dable = {'Dice': None}
+
+'''Control flow for each miner'''
+miner_dir = input(
+	"Please specify the miner specs you wish to classify (b/j/t/d): ")
 
 if (miner_dir.lower() == "b"):
-  # commit = 0
-  specs_dirs = f'../miners/ws/bdd-3'
-  for specs_d in os.listdir(specs_dirs):
-    specs_loc = f'../miners/ws/bdd-3/{specs_d}'
-    for s in os.listdir(specs_loc):
-      f = open(f'{specs_loc}/{s}', 'r')
-      f.read()
-      # keep track of files for debugging
-      # commit += 1
-      # if(commit % 3 == 0):
-      #   print('---')
-      # else:
-      #   print(s)
+	'''Setup initial table value for BDD'''
+	specs_dirs = f'../miners/ws/bdd-3'
+  # Get base name for the repository, this can be random
+	repo = re.sub(r'[0-9]', '', random.choice(os.listdir(specs_dirs)))
+	b_table['BDD'] = {repo: None}
+	b_table['BDD'][repo] = [{"persistent": None},
+								 {"intermittent": None},
+								 {"disappearing": None}]
+	# commit = 0
+	for specs_d in os.listdir(specs_dirs):
+		specs_loc = f'../miners/ws/bdd-3/{specs_d}'
+		for s in os.listdir(specs_loc):
+			f = open(f'{specs_loc}/{s}', 'r')
+
+			# keep track of files for debugging
+			# commit += 1
+			# if(commit % 3 == 0):
+			#   print('---')
+			# else:
+			#   print(s)
 elif (miner_dir.lower() == "j"):
-  print("")
+	print("")
--- scripts/sha.py
+++ scripts/sha.py
@@ -6,13 +6,14 @@ git_url = sys.argv[1]
 base_name = sys.argv[2]
 
 log = subprocess.run(
-    ["git","ls-remote", git_url], encoding="UTF-8", capture_output=True)
+    ["git", "ls-remote", git_url], encoding="UTF-8", capture_output=True)
 
 # split on tabs and lines and remove the SHA names
-shas = list(reduce(lambda a, s: a + s.split('\t'), log.stdout.split('\n')[:-1], []))[::2]
+shas = list(reduce(lambda a, s: a + s.split('\t'),
+			log.stdout.split('\n')[:-1], []))[::2]
 
 sha_file = open(f"{base_name}-sha.txt", "w+")
 for i in range(31):
-    sha_file.write(f"{git_url} {shas[i]} {base_name + str(i)}\n")
+	sha_file.write(f"{git_url} {shas[i]} {base_name + str(i)}\n")
 
 sha_file.close()
