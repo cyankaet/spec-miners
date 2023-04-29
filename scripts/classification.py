629462c88ec9ddcf63a1d33102801be0b82fa6d2
jpVinnie <janpaul.ramos@protonmail.com>
2023-04-29 19:01:02 -0400

    WIP parsing BDD

--- scripts/classification.py
+++ scripts/classification.py
@@ -21,15 +21,20 @@ spec_table = [{"persistent": None},
 			  {"disappearing": None}]
 
 ''' BDD file analysis'''
+
+
 def bdd_filter(file):
 	with open(file, "r") as f:
 		for line in f:
-			if ("Pattern:"  or "a=" or "b=" or "c=") not in line:
+			if ("Pattern:" or "a =" or "b =" or "c =") not in line:
 				break
 		for line in f:
 			yield line
 
+
 '''Control flow for each miner'''
+
+
 def bdd_analysis():
 	'''Setup initial table value for BDD'''
 	specs_dirs = f'../miners/ws/bdd-3'
@@ -48,15 +53,28 @@ def bdd_analysis():
 		specs_loc = f'../miners/ws/bdd-3/{specs_d}'
 		for s in os.listdir(specs_loc):
 			for line in bdd_filter(f'{specs_loc}/{s}'):
-				return None
+				regex = line
+				if "Pattern:" in line:
+					pat = re.sub("Pattern: ", '', regex)
+				if "a = " in line:
+					pat_a = re.sub("a = ", '', regex)
+				if "b = " in line:
+					pat_b = re.sub("b = ", '', regex)
+				if "c = " in line:
+					pat_c = re.sub("c = ", '', regex)
+					special_pat = pat+pat_a+pat_b+pat_c
+					print(special_pat)
 	print(b_table)
 	return b_table
+
 
 def javert_analysis():
 	return None
 
+
 def texada_analysis():
 	return None
+
 
 def dice_analysis():
 	return None
