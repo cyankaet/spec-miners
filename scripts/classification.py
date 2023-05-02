7b16d4b0dcee12a1a8ff5b5dd798b1bec77b601b
Kate Meuse <katemeuse@gmail.com>
2023-05-02 02:54:57 -0400

    add javert classification

--- scripts/classification.py
+++ scripts/classification.py
@@ -35,17 +35,36 @@ def bdd_filter(file):
 				spec[-1].append(line.strip())
 	return spec
 
+def javert_filter(file): # will need to count parens and append pattern
+	filename = re.sub(r'gol-javert-', '', file.split("/")[-1])
+	specs = []
+	with open(file, "r") as f:
+		for line in f:
+			if ("Specification") in line:
+				# if (not specs): #some spec has just been completed
+				# 	pat_str = "pattern (ab*c)*" if specs[-1].count("(") > 1 else "pattern (ab)*"
+				# 	specs[-1].insert(0,pat_str) #prepend with pattern as in bdd
+				# 	# currently not including pattern string bc info is encoded in string
+				specs.append([])
+				specs[-1].append(filename)
+			elif ("(" in line) or (")" in line):
+				specs[-1].append(line.strip())
+	# print(f"specs: {specs}")
+	return specs
+
+
+# outputs hashable string list where first two are pattern, folder #
 
 '''Control flow for each miner'''
 
-def bdd_fractional_pred(full_number=52, partial_number = 40):
-		full_set = bdd_analysis(full_number, False)
-		persisent_full_set = {k for k,v in full_set.items() if len(v) >= full_number}
-		partial_set = bdd_analysis(partial_number, False)
-		persisent_partial_set = {k for k,v in partial_set.items() if len(v) >= partial_number}
-		successful_infers = {spec for spec in persisent_partial_set if spec in persisent_full_set}
-		print(f"successful infers: {len(successful_infers)}")
-		print(f"num of persistent specs at {partial_number} commits: {len(persisent_partial_set)}")
+def fractional_pred(analysis_func, full_number=52, partial_number = 40):
+	full_set = analysis_func(full_number, False)
+	persisent_full_set = {k for k,v in full_set.items() if len(v) >= full_number}
+	partial_set = analysis_func(partial_number, False)
+	persisent_partial_set = {k for k,v in partial_set.items() if len(v) >= partial_number}
+	successful_infers = {spec for spec in persisent_partial_set if spec in persisent_full_set}
+	print(f"successful infers: {len(successful_infers)}")
+	print(f"num of persistent specs at {partial_number} commits: {len(persisent_partial_set)}")
 
 
 def bdd_analysis(num_commits, random):
@@ -85,8 +104,29 @@ def bdd_analysis(num_commits, random):
 	return spec_presence
 
 
-def javert_analysis():
-	return None
+def javert_analysis(num_commits, random):
+	specs_dir = f'../miners/ws/javert'
+	# Get base name for the repository, this repo can be chosen randomly
+	# repo = re.sub(
+	#     r'[0-9]', '', re.sub(r'gol-javert-', '', random.choice(os.listdir(specs_dir))))
+	selected_commits = os.listdir(specs_dir)[:num_commits] \
+		if not random else random.sample(os.listdir(specs_dir), num_commits)
+	spec_presence = {}
+	for spec_file in selected_commits:
+		for line in javert_filter(f"{specs_dir}/{spec_file}"):
+			filename = line[0] # pattern variable not currently inserted
+			rel_commit_number = re.search(r'\d+', filename).group()
+			line.remove(filename)
+			string_spec = ' '.join(line)
+			if string_spec in spec_presence:
+				spec_presence[string_spec].append(rel_commit_number)
+			else:
+				spec_presence[string_spec] = [rel_commit_number]
+	# print(f"spec pres: {spec_presence}")
+	# persistent_specs = list(filter(lambda x: len(x) > 51, spec_presence.values()))
+	# print(persistent_specs)
+	# print(len(persistent_specs))
+	return spec_presence
 
 
 def texada_analysis():
@@ -99,7 +139,8 @@ def dice_analysis():
 
 '''Output each table with categorized data'''
 # bdd_analysis()
-bdd_fractional_pred()
-javert_analysis()
+# fractional_pred(bdd_analysis)
+# javert_analysis()
+fractional_pred(javert_analysis)
 texada_analysis()
 dice_analysis()
